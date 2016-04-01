ba451f6a21f4b00ecefe694ad1b6c5c7fc62e2f7
Kang Lu <kang.lu@pvelocity.com>
2016-04-01 17:05:13 -0400

    Added package.json and fix some bugs.
    Added a test directory.

---
+++ pvserver.js
@@ -0,0 +1,229 @@
+"use strict";
+
+/*globals Buffer: true, require: true, module: true */
+
+//Get the header for an RPM API XML request as an array with optional sessionId inclusion.
+var getXmlReqHeader = function() {
+    var reqHeader = ['<?xml version="1.0" encoding="utf-8"?>',
+        '<PVRequest xmlns:xsd="http://www.w3.org/2001/XMLSchema" ',
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ',
+        'xmlns="http://pvelocity.com/rpm/pvrequest" '
+    ];
+    if (this.sessionId) {
+        reqHeader.push('sessionId="', this.sessionId, '"');
+    }
+    reqHeader.push('>');
+
+    return reqHeader;
+};
+
+// Create an XML request string for the RPM API, note that the parameters are in XML.
+var buildXmlReqStr = function(operation, parameters) {
+    var req;
+
+    req = getXmlReqHeader.call(this);
+    req.push('<Operation><Name>', operation, '</Name>', '<Params>');
+    if (parameters) {
+        // don't send raw '&', but don't change '&amp;', '&lt;', etc.
+        req.push(
+            parameters.replace(/&/g, "&amp;")
+            .replace(/&amp;(amp|lt|gt|quot);/g, "&$1;")
+        );
+    }
+    req.push('</Params></Operation></PVRequest>');
+
+    return req.join('');
+};
+
+function PVServerAPI() {
+
+    this.user = null;
+    this.role = null;
+    this.hostName = "localhost";
+    this.hostPort = 80;
+    this.urlPath = "/RPM";
+    this.urlScheme = "http";
+    this.http = (this.urlScheme === 'http' ? require('http') : require('https'));
+    this.querystring = require('querystring');
+
+    this.configPath = "/PE/DKC/";
+
+    this.device = "HTML5";
+    this.sessionId = null;
+    this.cookie = null;
+    this.version = "3.4";
+    this.privileges = {};
+    this.timeOut = 30 * 60;
+
+    this.UomMap = {
+        A: "Sq.Ft.",
+        C: "$",
+        L: "Ft",
+        T: "Hr",
+        W: "lb"
+    };
+}
+
+PVServerAPI.prototype.setHostURL = function(urlString) {
+    var refUrl = urlString;
+    if (typeof urlString === "string") {
+        var url = require('url');
+        refUrl = url.parse(urlString);
+    }
+
+    var server = this;
+
+    server.hostName = refUrl.hostname || server.hostName;
+    server.urlPath = (refUrl.pathname || server.urlPath) + "/RPM";
+    server.urlPath = server.urlPath.replace("//", "/");
+    server.urlScheme = refUrl.protocol || server.urlScheme;
+    server.urlScheme = server.urlScheme.replace(":", "");
+
+    server.http = null;
+    if (server.urlScheme === 'https') {
+        server.http = require('https');
+        server.hostPort = 443;
+    } else {
+        server.http = require('http');
+        server.hostPort = 80;
+    }
+    server.hostPort = refUrl.port || server.hostPort;
+}
+
+/**
+ * Given a JSON response object, determine if the result status is okay or not
+ *
+ * @param {Object}
+ *            The response object
+ * @return {boolean}
+ *            true if it is okay, false otherwise
+ */
+PVServerAPI.prototype.isOkay = function(code) {
+    return (code === "RPM_PE_STATUS_OK");
+};
+
+PVServerAPI.prototype.isStatusOkay = function(status) {
+    return ((status && status.Code) ? this.isOkay(status.Code.text) : false);
+};
+
+PVServerAPI.prototype.isJsonOkay = function(json) {
+    var status = null;
+    if (json && json.PVResponse) {
+        status = json.PVResponse.PVStatus;
+    }
+    return this.isStatusOkay(status);
+};
+
+/**
+ * This is a convenience function that will send an async RPM API request to the
+ * server.
+ *
+ * @param {String}
+ *            operation The name of the operation
+ * @param {String}
+ *            parameters An XML string containing the contents of the parameters
+ * @param {Function}
+              The call back function to execute when the async request completes (err, result)
+ */
+PVServerAPI.prototype.sendRequestAsync = function(operation, parameters, completionCallback) {
+
+    var server = this;
+    var requestStr = buildXmlReqStr.call(server, operation, parameters);
+
+    var post_data = this.querystring.stringify({
+        "dataformat": "json",
+        "request": requestStr
+    });
+
+    var headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(post_data),
+        'Connection': 'Keep-Alive',
+        'X-PVClient-Version': this.version,
+        "X-PVClient-Platform": this.device
+    };
+
+    if (this.cookie) {
+        headers.Cookie = this.cookie[0];
+    }
+
+    var post = this.http.request({
+            host: this.hostName,
+            port: this.hostPort,
+            path: this.urlPath,
+            method: 'POST',
+            headers: headers
+        },
+        function(res) {
+            res.setEncoding('utf8');
+            var data = "";
+            res.on('data', function(chunk) {
+                data += chunk;
+
+            });
+            res.on('end', function() {
+                var json = null;
+                try {
+                    json = JSON.parse(data);
+                } catch (err) {
+                    json = data;
+                }
+                var status = (json && json.PVResponse) ? json.PVResponse.PVStatus : null;
+                if (server.isStatusOkay(status)) {
+                    server.sessionId = status.SessionId.text;
+                    if (operation === 'Login' && res.headers['set-cookie']) {
+                        server.cookie = res.headers['set-cookie'];
+                    }
+                }
+                completionCallback.call(server, status ? status.Code.text : null, json);
+            });
+        }
+    );
+
+    post.write(post_data);
+    post.end();
+};
+
+PVServerAPI.prototype.login = function(user, password, credKey, completionCallback) {
+
+    var params = ['<User>', user, '</User>'];
+    if (password) {
+        params.push('<Password>', password, '</Password>');
+    }
+    if (credKey) {
+        params.push('<CredentialKey>', credKey, '</CredentialKey>');
+    }
+    params.push('<TimeOut>', this.timeOut.toString(), '</TimeOut>');
+    params.push('<DeviceName>', this.device, '</DeviceName>');
+
+    this.sendRequestAsync("Login", params.join(""), function(err, json) {
+        if (this.isOkay(err)) {
+            this.user = json.PVResponse.PVStatus.User.text;
+            this.role = json.PVResponse.PVStatus.UserGroup.text;
+        }
+        completionCallback.call(this, err, json);
+    });
+};
+
+PVServerAPI.prototype.logout = function(completionCallback) {
+
+    this.sendRequestAsync("Logout", null, function(err, json) {
+        this.user = null;
+        this.role = null;
+        this.cookie = null;
+        this.sessionId = null;
+        completionCallback.call(this, err, json);
+    });
+};
+
+var privateServer = new PVServerAPI();
+
+// Only export what we want
+
+module.exports = {
+    setHostURL: privateServer.setHostURL.bind(privateServer),
+    isOkay: privateServer.isOkay.bind(privateServer),
+    sendRequestAsync: privateServer.sendRequestAsync.bind(privateServer),
+    login: privateServer.login.bind(privateServer),
+    logout: privateServer.logout.bind(privateServer)
+};
---
+++ test/index.js
@@ -0,0 +1,108 @@
+#!/usr/bin/env node
+
+"use strict";
+
+/*globals require: true */
+
+var queryParams = ['<Currency>USD</Currency>',
+    '<ProfitModel>PipelineProduct</ProfitModel>',
+    '<Category>Sales</Category>',
+    '<Groups>',
+    '<Group name="Res1">PV_Industry</Group>',
+    '</Groups>',
+    '<Fields>',
+    '<Field>PV_Order_Margin</Field>',
+    '</Fields>',
+    '<SearchCriteria>',
+    '<DateRange ignoreBaseQuery="true">',
+    '<From>',
+    '<Year>1000</Year>',
+    '<Month>01</Month>',
+    '</From>',
+    '<To>',
+    '<Year>2999</Year>',
+    '<Month>06</Month>',
+    '</To>',
+    '</DateRange>',
+    '</SearchCriteria>'
+].join("");
+
+var doWork = function(err, json) {
+
+    if (!this.isOkay(err)) {
+        console.log("Login Failed.");
+        return;
+    }
+
+    console.log(JSON.stringify(json));
+
+    this.sendRequestAsync('Query', queryParams, function(err, json) {
+
+        console.log(JSON.stringify(json));
+
+        if (this.isOkay(err)) {
+
+            this.logout(function(err, json) {
+
+                console.log(JSON.stringify(json));
+
+                // Invalid session test
+                this.sendRequestAsync('Query', queryParams, function(err, json) {
+                    console.log(JSON.stringify(json));
+                    if (err === "RPM_PE_INVALID_SESSION") {
+                        console.log("Test Successful.");
+                    } else {
+                        console.log("Test Failed.");
+                    }
+                });
+            });
+
+        } else {
+            console.log("Query Failed.");
+        }
+    });
+};
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var userid = null;
+var passwd = null;
+var hostname = null;
+
+rl.question('Host URL (e.g. http://xxxx.pvelocity.com): ', (answer) => {
+    if (answer) {
+        hostname = answer;
+        rl.question('User Id: ', (answer) => {
+            if (answer) {
+                userid = answer;
+                rl.question('Password: ', (answer) => {
+                    if (answer) {
+                        var pv = require('./pvserver');
+                        passwd = answer;
+
+                        // Login into the server
+                        pv.setHostURL(hostname);
+                        pv.login(userid, passwd, null, doWork.bind(pv));
+                    } else {
+                        console.log('Password is required.');
+                    }
+                    rl.close();
+                });
+            } else {
+                console.log('User Id is required.');
+                process.exit(1);
+                rl.close();
+            }
+        });
+    } else {
+        console.log('Host is required.');
+        process.exit(1);
+        rl.close();
+
+    }
+});
